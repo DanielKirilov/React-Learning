90023ef41f796fddb65b37d526e4ff7abf3cfa86
DanielKirilov <dancata08@gmail.com>
2025-02-26 21:54:24 +0200

    Added counter

--- React-Components-DeepDive/src/App.jsx
+++ React-Components-DeepDive/src/App.jsx
@@ -5,10 +5,19 @@ import './App.css'
 function App() {
 
   const [numbers, setNumbers] = useState([1, 2, 3, 4, 5])
+  const [count, setCount] = useState(0)
+
 
   function onDelete() {
     setNumbers(oldState => oldState.slice(0, oldState.length - 1))
   }
+  function onAdd() {
+    setCount(setCount => setCount + 1);
+  }
+  
+  useEffect(() => {
+    console.log("count");
+  }, [count]);
 
   useEffect(() => {
     console.log("Mount component");
@@ -25,6 +34,8 @@ function App() {
   return (
     <>
       <div>
+        <h2>Count : {count}</h2>
+        <button onClick={onAdd}>+</button>
         <ul>
           {numbers.map((number, index) => <li key={index}>{number}</li>)}
         </ul>
