a118607fe1e8761b45a063cf21f974f5aef7cc20
DanielKirilov <dancata08@gmail.com>
2025-03-02 20:05:05 +0200

    Finished all html and css setups

--- User-List/client/src/App.jsx
+++ User-List/client/src/App.jsx
@@ -1,11 +1,19 @@
-import Header from './components/Header'
 import './styles.css'
-Header
+import Header from './components/Header'
+import Footer from './components/Footer'
+import UserList from './components/UserList'
 
 function App() {
   return (
     <div>
-      <Header/>
+
+      <Header />
+
+      <main className='main'>
+        <UserList />
+      </main>
+
+      <Footer />
     </div>
   )
 }
