edd37907bbfb59972afbc32b037fc0ad40b9825e
DanielKirilov <dancata08@gmail.com>
2025-03-11 18:49:02 +0200

    Added component

---
+++ React-Uncontrolled-Forms/src/components/UncontorlledForm.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+
+export default function UncontrolledForm() {
+    return (
+        <Fragment>
+            <h1>Uncontrolled Form</h1>
+
+            <form></form>
+        </Fragment >
+    )
+
+}
