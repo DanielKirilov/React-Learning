86edef7e97258ded5b18ba772194dfad39b496fc
DanielKirilov <dancata08@gmail.com>
2025-03-15 21:34:41 +0200

    Created simple forms

--- React-Controlled-Forms/src/App.jsx
+++ React-Controlled-Forms/src/App.jsx
@@ -4,7 +4,22 @@ function App() {
 
   return (
     <>
-      
+      <form >
+        <div>
+        <label htmlFor="username">Username  </label>
+          <input type="text" />
+        </div>
+
+        <div>
+        <label htmlFor="age">Age  </label>
+          <input type="number" />
+        </div>
+
+        <div>
+        <label htmlFor="password">Password  </label>
+          <input type="password" />
+        </div>
+      </form>
     </>
   )
 }
