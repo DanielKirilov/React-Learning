ecc0c7edab6698cbef9a88d88fc9a13ebfad58ea
DanielKirilov <dancata08@gmail.com>
2025-03-20 20:51:07 +0200

    Simple  controlled form

--- React-Controlled-Forms/src/App.jsx
+++ React-Controlled-Forms/src/App.jsx
@@ -1,27 +1,49 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  let [username, setUsername] = useState("")
+  let [password, setPassword] = useState("")
+
+  const usernameChangeHandler = (e) => {
+    console.log(e.currentTarget.value);
+    setUsername(e.currentTarget.value)
+  }
+
+  const passwordChangeHandler = (e) => {
+    console.log(e.currentTarget.value);
+    setPassword(e.currentTarget.value)
+  }
 
   return (
     <>
-      <form >
-        <div>
-        <label htmlFor="username">Username  </label>
-          <input type="text" />
-        </div>
+      <h1>Controlled Form</h1>
 
+      <form>
         <div>
-        <label htmlFor="age">Age  </label>
-          <input type="number" />
+          <label
+            htmlFor="username">Username </label>
+          <input
+            onChange={usernameChangeHandler}
+            type="text"
+            name="username"
+            id="username" />
         </div>
-
         <div>
-        <label htmlFor="password">Password  </label>
-          <input type="password" />
+          <label htmlFor="username">Password  </label>
+          <input
+            onChange={passwordChangeHandler}
+            type="password"
+            name="password"
+            id="password" />
+        </div>
+        <div>
+          <input type="submit" value="Register" />
         </div>
       </form>
     </>
   )
+
 }
 
 export default App
