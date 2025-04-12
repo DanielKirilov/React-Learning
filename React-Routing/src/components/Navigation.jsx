e82658775edc5d7fdd5f9abe8c609a1e4203a48b
DanielKirilov <dancata08@gmail.com>
2025-04-12 17:06:36 +0300

    Now using Link
    Pages do not reload now

--- React-Routing/src/components/Navigation.jsx
+++ React-Routing/src/components/Navigation.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default Navigation = () => {
     return (
         <nav>
-            <a href="/">Home </a>
-            <a href="/about">About </a>
-            <a href="/contact">Contact </a>
+            <Link to="/" >Home </Link>
+            <Link to="/about" >About </Link>
+            <Link to="/contact" >Contact </Link>
         </nav>
     )
 }
