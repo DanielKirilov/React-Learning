08b2c634a909096263a13c1667556042b1452ddb
DanielKirilov <dancata08@gmail.com>
2025-04-12 16:55:27 +0300

    Added 3 simple pages

---
+++ React-Routing/src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+export default Navigation = () => {
+    return (
+        <nav>
+            <a href="/">Home </a>
+            <a href="/about">About </a>
+            <a href="/contact">Contact </a>
+        </nav>
+    )
+}
